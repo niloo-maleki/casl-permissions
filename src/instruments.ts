eb398c8262c4f628ba9d236bd2cb7c70ab143de7
niloo-maleki <111730218+niloo-maleki@users.noreply.github.com>
2024-11-19 15:19:09 +0330

    fix: add new file for sentry config

---
+++ src/instruments.ts
@@ -0,0 +1,17 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://d555823c1c72cf8180a3e55333aa0640@o4507763561136128.ingest.us.sentry.io/4508030355898368",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.browserProfilingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    // tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
